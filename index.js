12c574a825b15ad833fa3b57869377ed907a67e8
Daniella Rono <daniellarono@gmail.com>
2024-07-14 21:00:52 +0300

    all tests are passing

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate'){
+return `This Saturday, I want to ${activity}!`
+}
+function mondayWork ( answer = "go to the office"){
+    return `This Monday, I will ${answer}.`
+}
+function wrapAdjective (wrapper){
+    return function(adjective){
+        return`You are ${wrapper}${adjective}${wrapper}!`
+    }
+}
